ae30be71b4d1680bbb08748c4ee8d50900fe632e
vsdev543 <ozi.cse20@gmail.com>
2022-02-01 11:44:36 +0530

    functions defined for invers_ >> lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  
+  get<-function() x
+  setInv<-function(invX){inv<<-invX}
+  getInv<-function(){inv}
+  
+  list(
+    set=set,
+    get=get,
+    setInv=setInv,
+    getInv=getInv
+  )
+  
 }
 
 
@@ -12,4 +29,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  k<-x$getInv()
+  if(!is.null(k)){
+    message("getting cached data")
+    return(k)
+  }
+  
+  data<-x$get()
+  invx<-solve(data)
+  x$setInv(invX = invx)
+  
+  invx
 }
